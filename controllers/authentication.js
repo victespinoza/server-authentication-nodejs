1a627dc04fa61edb286c752be2b0908670efd849
Victor Espinoza <vespinoza@technisys.com>
2019-04-10 00:19:23 -0300

    update authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,15 @@
+const jwt = require('jwt-simple');
 const User = require('../models/user');
+const config = require('../config');
+
+function tokenForUser(user){
+    const timestamp = new Date().getTime();
+    return jwt.encode({sub: user.id, iat: timestamp},config.secret);
+}
+
+exports.signin = function(req, res, next){
+    res.send({token: tokenForUser(req.user)});
+}
 
 exports.signup = function(req, res, next){
     const email = req.body.email;
@@ -19,7 +30,7 @@ exports.signup = function(req, res, next){
         });
         user.save(function(err){
             if(err){return next(err);}
-            res.json(user);
+            res.json({token: tokenForUser(user)});
         });
     });
 
